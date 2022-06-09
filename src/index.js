12bf70771e7e5cb5da479f797a256c501a0adc2d
Luis Ariza <luismanuelariza@hotmail.com>
2022-06-09 15:45:02 -0500

    Agregando imagenes con JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const max = 122;
+const min = 1;
+const random = () => Math.floor(Math.random() * (max - min)) + min;
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+  const container = document.createElement("figure");
+  container.className = "p-4";
+
+  const imagen = document.createElement("img");
+  imagen.className = "mx-auto";
+  imagen.width = "320";
+  imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  container.appendChild(imagen);
+
+  return container;
+};
+
+const mountNode = document.getElementById("images");
+const addBoton = document.querySelector("button");
+
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.appendChild(newImage);
+};
+
+addBoton.addEventListener("click", addImage);
